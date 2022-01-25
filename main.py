54bc84211a1362731503ba4e2cb6081f9fb3ffea
ayush93 <ayushbhesaniya@gmail.com>
2022-01-24 22:56:02 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# this is main file 
+## below are the things it should gate as of now.
+# 1) path to monthly cost list.
+# 2) what all lists needs to be tracked.
+
+
+class main:
+    def __init__(self):
+        print ("omm namhh shivay")
+
+
+print ("New class generated")
